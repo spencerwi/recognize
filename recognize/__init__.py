758f292657f9a78e344a66a5be147a3a0fe06be3
spencerwi <spencerwi@gmail.com>
2013-07-27 21:40:21 -0400

    Stubs for issue #1 and #2.

--- recognize/__init__.py
+++ recognize/__init__.py
@@ -1,12 +1,42 @@
+"""recognize is a simple python library for vocabulary recognition.
+
+It uses a configurable Levenshtein distance threshold to determine which words of a given vocabulary are likely matches.
+
+*Usage*
+
+For use with recognize, your vocabulary should be an iterable of string-like objects.
+
+Then, import recognize and just call the recognize() method with your word to match, your vocabulary, and optionally your desired Levenshtein distance threshold for matching (the default is 3):
+
+    >>> from recognize import recognize
+    >>> recognize("ana", vocabulary=["banana", "grape", "orange", "cucumber"], distance=3)
+    set(['banana'])
+
+To avoid having to pass your vocabulary and threshold into every call to recognize(), you can also initialize a Recognizer object with these parameters:
+
+    >>> from recognize import Recognizer
+    >>> r = Recognizer(vocabulary=["banana", "grape", "orange", "cucumber"], distance=3)
+    >>> r.recognize("ana")
+    set(['banana'])
+    >>> r.recognize("oge")
+    set(['orange'])
+"""
+
 vocabulary = []
 threshold = 3
+
 
 class Recognizer(object):
     def __init__(self, vocabulary = [], distance = 3):
         self.vocabulary = vocabulary
         self.distance = distance
 
+    def vocab_from_file(self, filename, delimiter=None):
+        """Reads vocabulary from a file. Delimiter defaults to all whitespace."""
+        # TODO: finish this
+        raise NotImplementedError
 
+    # TODO: move this to a "algorithms.distance" package, refactor to "get_distance()" method
     @staticmethod
     def levenshtein(seq1, seq2):
         """Calculates the levenshtein distance between two strings."""
